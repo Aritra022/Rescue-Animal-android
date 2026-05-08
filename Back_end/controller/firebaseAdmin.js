9d03fbf2893fdbea77d315307fc8016c18225c4f
Aritra Bhunia <your-email@gmail.com>
2026-05-08 14:00:48 +0530

    Updated Render environment configuration

--- Back_end/controller/firebaseAdmin.js
+++ Back_end/controller/firebaseAdmin.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount =process.env.SERVIECE_ACCOUNT_KEY;
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
